543654b0071f233bc6bbbbd1d6bbb9c17c7baa24
Yasushi Tamura <yasushitamura@YasushinoMacBook-Air.local>
2022-09-26 10:50:36 +0900

    GIthubとの接続確認用

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/kataras/iris/v12"
+
+func main() {
+	app := iris.New()
+
+	app.Handle("GET", "/", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "ping"})
+	})
+
+	app.Listen(":8080")
+}
